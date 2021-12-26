15b9e2ce199e6d465f947291a44da2aca730424c
Sergey <33506866+timshinsergey@users.noreply.github.com>
2021-12-26 20:20:46 +0400

    creating fake data, feedback list component

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, reiciendis mollitia, facere reprehenderit consequatur nobis sit quibusdam consectetur eveniet, fugiat adipisci iste. Voluptates necessitatibus id at delectus tenetur, veniam quidem.'
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, reiciendis mollitia, facere reprehenderit consequatur nobis sit quibusdam consectetur eveniet, fugiat adipisci iste. Voluptates necessitatibus id at delectus tenetur, veniam quidem.'
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, reiciendis mollitia, facere reprehenderit consequatur nobis sit quibusdam consectetur eveniet, fugiat adipisci iste. Voluptates necessitatibus id at delectus tenetur, veniam quidem.'
+	},
+]
+
+export default FeedbackData
