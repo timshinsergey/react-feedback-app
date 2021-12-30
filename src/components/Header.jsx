f13b82dce87f13d6bddfe573c61b7675c20009f1
Sergey <33506866+timshinsergey@users.noreply.github.com>
2021-12-30 14:49:05 +0400

    Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types'
 
 function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
         <h2>{text}</h2>
       </div>
