fa3837284abc6925fdc3de8100a31c53177288e5
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-02 14:56:04 +0400

    creating Button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react/cjs/react.development'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
@@ -17,7 +18,7 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
