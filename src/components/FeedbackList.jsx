318cc457347451d4a9a94ec43605003d1f3c5224
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-02 19:09:27 +0400

    retrieve a list of feedback from the feedback context

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import { motion, AnimatePresence } from 'framer-motion'
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -22,16 +25,6 @@ function FeedbackList({ feedback, handleDelete }) {
       </AnimatePresence>
     </div>
   )
-}
-
-FeedbackList.propTypes = {
-  item: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      number: PropTypes.number.isRequired,
-    })
-  ),
 }
 
 export default FeedbackList
