ce2f6988866ccbb0d041cfc04c4280e4b8529c46
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-02 21:45:03 +0400

    adding fetch feedback from server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,15 +1,27 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
-import FeedbackData from '../data/FeedbackData'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState(FeedbackData)
+	const [isLoading, setIsLoading] = useState(true)
+	const [feedback, setFeedback] = useState([])
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false
 	})
+
+	useEffect(() => {
+		fetchFeedback()
+	}, [])
+
+	const fetchFeedback = async () => {
+		const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+		const data = await response.json()
+
+		setFeedback(data)
+		setIsLoading(false)
+	}
 
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4()
@@ -39,6 +51,7 @@ export const FeedbackProvider = ({ children }) => {
 			value={{
 				feedback,
 				feedbackEdit,
+				isLoading,
 				deleteFeedback,
 				addFeedback,
 				updateFeedback,
