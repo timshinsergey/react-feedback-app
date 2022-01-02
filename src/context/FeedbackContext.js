27b9f81ae26ccee2f148aa54f68ad5604457e120
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-02 19:23:33 +0400

    transfer of feedback functions

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,19 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from 'react'
+import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: 'This items is from context',
-			rating: 10
-		},
-	])
+	const [feedback, setFeedback] = useState(FeedbackData)
+
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
+
+	const deleteFeedback = (id) => {
+		if (window.confirm('Are you sure you want to delete?')) {
+			setFeedback(feedback.filter((item) => item.id !== id))
+		}
+	}
 	return (
 		<FeedbackContext.Provider
 			value={{
 				feedback,
+				deleteFeedback,
+				addFeedback,
 			}}>
 			{children}
 		</FeedbackContext.Provider>
